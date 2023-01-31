c1a8c6acc4a2603041f8deab227a9998051d99f4
Cyprien Rimbaud <76480360+Sidarzo@users.noreply.github.com>
2023-01-31 20:54:53 +0100

    TDD Palindrome Test qui passent

--- OHCE/Ohce.cs
+++ OHCE/Ohce.cs
@@ -1,8 +1,18 @@
 ﻿
+using System.Text;
+
 namespace OHCE
 {
     public class Ohce
     {
-        public string Traitement(string mot) { return new string(mot.Reverse().ToArray() + "Bien dit !"); }
+        public string Traitement(string mot) {
+            var builderString = new StringBuilder();
+            var miroir = new string(mot.Reverse().ToArray());
+
+            builderString.Append(miroir);
+
+            if(miroir == mot) { builderString.Append("Bien dit !"); };
+            return builderString.ToString();
+        }
     }
 }
