520e76400a3cd9beb6f128dab973fd791b798df7
Cyprien Rimbaud <76480360+Sidarzo@users.noreply.github.com>
2023-01-31 20:38:16 +0100

    Miroir en mode TDD 1

--- OHCE/Ohce.cs
+++ OHCE/Ohce.cs
@@ -1,27 +1,8 @@
-﻿using OHCE.Langues;
-
+﻿
 namespace OHCE
 {
     public class Ohce
     {
-        private readonly ILangue _langue;
-        private readonly MomentDeLaJournée _momentDeLaJournée;
-
-        public Ohce(ILangue langue, MomentDeLaJournée momentDeLaJournée)
-        {
-            _langue = langue;
-            _momentDeLaJournée = momentDeLaJournée;
-        }
-
-        public string Miroir(string chaîne)
-        {
-            var miroir = new string(chaîne.Reverse().ToArray());
-            var estUnPalindrome = miroir.Equals(chaîne);
-
-            return _langue.Saluer(_momentDeLaJournée)
-                   + miroir
-                   + (estUnPalindrome ? _langue.BienDit : string.Empty)
-                   + _langue.Acquittance;
-        }
+        public string Traitement(string mot) { return "laval"; }
     }
 }
