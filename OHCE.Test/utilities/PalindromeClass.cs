d1af1aabff79bbe5fd370c6cea39b4958904f299
Cyprien Rimbaud <76480360+Sidarzo@users.noreply.github.com>
2023-01-31 21:33:52 +0100

    Ajout  palindrome class

---
+++ OHCE.Test/utilities/PalindromeClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OHCE.Test.utilities
+{
+    public class PalindromeClass : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { new LangueFrancais(), "laval", new LangueFrancais().BienDit };
+            yield return new object[] { new LangueAnglais(), "kayak", new LangueAnglais().BienDit };
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
